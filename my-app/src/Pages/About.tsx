488550afdd178a03956afced21663516f56ed456
TLRKiliann <philogenie@protonmail.com>
2022-09-06 19:44:21 +0200

    style at work

--- my-app/src/Pages/About.tsx
+++ my-app/src/Pages/About.tsx
@@ -1,23 +1,25 @@
+import { Container } from 'react-bootstrap';
 import koalaContact from "../assets/images/koala-3d.jpg";
 import '../styles/about.scss';
 
 
 export const About = () => {
   return (
-    <div className="text-hide" >
-      <img 
-        src={koalaContact} style={{width: "84%", position: "fixed"}}
-        className="koala--bg"
-        alt="koala-3d(about)" />
-      
-      <h6 style={{fontSize: "3rem"}}>About Us</h6>
+    <div className="text-hide">
+      <Container className="sm-4">
+        <img 
+          src={koalaContact}
+          className="koala--bg"
+          alt="koala-3d(about)" />
+        
+        <h6 style={{fontSize: "3rem"}}>About Us</h6>
 
-      <div className="divanim--about">
-        <p className="anim--about">We improved our work with you</p>
-        <p className="anim--about">Severals services</p>
-        <p className="anim--about">Since 1997</p>
-      </div>
-
+        <div className="divanim--about">
+          <p className="anim--about">We improved our work with you</p>
+          <p className="anim--about">Severals services</p>
+          <p className="anim--about">Since 1997</p>
+        </div>
+      </Container>
     </div>
   );
 }
